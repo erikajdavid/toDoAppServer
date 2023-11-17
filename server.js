4c259455bcf5d922f351c87a5eedb062d434dc4a
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-11-17 12:17:16 -0500

    added cookie parser dependcy and imported into server.js / this is third party middleware

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
 //this is built-in middleware that will allow the app to receive and parse JSON data. 
 app.use(express.json())
+
+app.use(cookieParser());
 
 //express.static is built-in middleware
 app.use('/', express.static(path.join(__dirname, '/public')))
