febe14e5326b726a509b2d0b396a6c91b4b8f6bd
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-18 12:09:17 -0500

    access/refresh tokens initiated and login/refresh controllers completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,16 +16,85 @@ const login = asyncHandler(async(req, res) => {
         return res.status(401).json({ message: 'Unauthorized' })
     }
 
-    //if there is username match, verify password
+    //if there is user exists, verify password
     const match = await bcrypt.compare(password, foundUser.password)
 
+    //if the password isn't valid, return error
     if (!match) {
         return res.status(401).json({ message: 'Unauthorized' })
     }
 
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '10s'}
+    )
+
+    const refreshToken = jwt.sign(
+        {
+            "username": foundUser.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '1d'}
+    )
+
+    //create secure cookie with refresh token
+
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, //accessible only by web server
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie
+        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry set to match refresh token
+    })
+
+    //The client application receives the access token. 
+    //The server sets the cookie. 
+    //The client application with react never handles the refresh token inside of this cookie, but this ensures that when react sends a request to the refresh endpoint, the cookie is sent along with it. 
+    res.json({ accessToken }) 
 })
 
 const refresh = (req, res) => {
+    const cookies = req.cookies
+
+    if(!cookies?.jwt) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const refreshToken = cookies.jwt
+
+    //now you need to verify the token
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET, 
+        asyncHandler(async(err, decoded) => {
+            if (err) {
+                return res.status(403).json({ message: 'Forbidden' })
+            }
+
+            const foundUser = await User.findOne({ username: decoded.username })
+
+            if(!foundUser) {
+                return res.status(401).json({ message: 'Unauthorized' })
+            }
+
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo": {
+                        "username": foundUser.username
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '10s'}
+            )
+
+            res.json({ accessToken })
+        })
+    )
 
 }
 
